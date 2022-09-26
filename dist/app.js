989dc2614e9829db9cfa9c5758c6b464d72ab126
brian_lin <lcylcy66666@gmail.com>
2022-09-26 17:35:06 +0800

    render

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,14 @@ class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById("project-input");
         this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
+const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
