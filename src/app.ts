a80b9d2234e3d1936669895c33a285f26503b21a
brian_lin <lcylcy66666@gmail.com>
2022-09-26 15:27:04 +0800

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+  }
+}
